f63c0dbead9c5a4dd7218cf66c9da477091108d7
James <jamessoh11@gmail.com>
2024-01-05 14:01:27 +0800

    First commit

---
+++ src/main/java/com/eventplanner/demo/auth/controller/AuthController.java
@@ -0,0 +1,20 @@
+package com.eventplanner.demo.auth.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eventplanner.demo.auth.model.UserCredentials;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+public class AuthController {
+
+  @PostMapping("path")
+  public String postMethodName(@RequestBody UserCredentials userCredentials) {
+    return "Some jwt for user " + userCredentials.getEmail();
+  }
+
+}
